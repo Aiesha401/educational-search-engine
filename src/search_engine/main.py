fa381fea1c4f788a37c3987ce0e6cca2d40048e1
Aiesha <aieshashaik4023@gmail.com>
2026-09-17 18:24:22 +0530

    changed main.py by mistake

--- src/search_engine/main.py
+++ src/search_engine/main.py
@@ -1,21 +1,10 @@
-# from src.search_engine.cli import SearchEngineCLI
+from src.search_engine.cli import SearchEngineCLI
 
 
-# def main():
-#     cli = SearchEngineCLI()
-#     cli.run()
+def main():
+    cli = SearchEngineCLI()
+    cli.run()
 
 
-# if __name__ == "__main__":
-#     main()
-
-from src.search_engine.document import Document
-from src.search_engine.indexer import Indexer
-
-indexer = Indexer()
-
-indexer.index(Document("1", "the dog chased the dog"))
-indexer.index(Document("2", "the dog"))
-
-print(vars(indexer.inverted_index["dog"]["1"]))
-# print(indexer.inverted_index)
+if __name__ == "__main__":
+    main()
